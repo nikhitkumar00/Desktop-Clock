1777591ae90fd47506f684fb06e3158b7fdc9a83
Nikhit Kumar <nikhitkumar00@gmail.com>
2023-10-02 19:55:14 +0530

    changed to typescript

--- index.ts
+++ index.ts
@@ -1,19 +1,23 @@
-setInterval(function () {
-	var date = new Date();
-	var hour = date.getHours();
-	var minute = date.getMinutes();
-	var second = date.getSeconds();
-	var meridian;
+setInterval(() => {
+	const date: Date = new Date();
+	let hour: number | string = date.getHours();
+	let minute: number | string = date.getMinutes();
+	let second: number | string = date.getSeconds();
+	let meridian: string;
+
 	hour = hour < 10 ? "0" + hour : hour.toString();
 	minute = minute < 10 ? "0" + minute : minute.toString();
 	second = second < 10 ? "0" + second : second.toString();
+
 	meridian = hour < "12" ? "AM" : "PM";
 	hour = hour > "12" ? (parseInt(hour) - 12).toString() : hour.toString();
 	hour = hour === "00" ? "12" : hour;
-	var hourElement = document.getElementById("hour");
-	var minuteElement = document.getElementById("min");
-	var secondElement = document.getElementById("sec");
-	var meridianElement = document.getElementById("mer");
+
+	const hourElement: HTMLElement | null = document.getElementById("hour");
+	const minuteElement: HTMLElement | null = document.getElementById("min");
+	const secondElement: HTMLElement | null = document.getElementById("sec");
+	const meridianElement: HTMLElement | null = document.getElementById("mer");
+
 	if (hourElement) hourElement.innerText = hour;
 	if (minuteElement) minuteElement.innerText = minute;
 	if (secondElement) secondElement.innerText = second;
